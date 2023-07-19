80de3a0320956be1dbc06f5ad9bb7fb0283a0a82
xcssa <1454643841@qq.com>
2023-07-20 02:33:17 +0800

    添加项目文件。

---
+++ CPShare/Controls/CodeTextBlock.cs
@@ -0,0 +1,81 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CPShare.Models;
+using CPShare.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextMateSharp.Grammars;
+using TextMateSharp.Registry;
+using TextMateSharp.Themes;
+
+namespace CPShare.Controls
+{
+    public class CodeTextBlock : Control
+    {
+        public ObservableCollection<CodeToken> Tokens { get; set; }
+
+        public CodeTextBlock()
+        {
+            Tokens = new ObservableCollection<CodeToken>(@"
+    using System;
+    using System.IO;
+
+    namespace CPShare
+    {
+        public class Test
+        {
+            public static void Test(string value)
+            {
+                // xxx
+                /* yyyyy
+                   aaaaa */
+   
+                Console.WriteLine(""Hello CPShare"");
+                Console.WriteLine($""Hello {value}"");
+            }
+        }
+    }
+".ToCodeTokens(".cs"));
+        }
+
+        public sealed override void Render(DrawingContext context)
+        {
+            if (Tokens != null)
+            {
+                var i = 0.0;
+                var j = 0;
+                foreach (var token in Tokens)
+                {
+                    foreach (var tokenChar in token.Text)
+                    {
+                        if (tokenChar == '\r')
+                            continue;
+                        if (tokenChar == '\n')
+                        {
+                            i = 0;
+                            j++;
+                            continue;
+                        }
+                        if (tokenChar == ' ')
+                        {
+                            i += 5;
+                            continue;
+                        }
+                        var tokenStr = tokenChar.ToString();
+                        var ft = new FormattedText(tokenStr, CultureInfo.CurrentUICulture, FlowDirection.LeftToRight, Typeface.Default, 20, Brush.Parse(token.ForegroundColor.ToString()));
+                        context.DrawText(ft, new Point(i, j * ft.Height));
+                        i += ft.Width;
+                    }
+                }
+            }
+
+            base.Render(context);
+        }
+    }
+}
---
+++ CPShare/Models/CodeToken.cs
@@ -0,0 +1,160 @@
+﻿using Avalonia.Media;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextMateSharp.Grammars;
+using TextMateSharp.Registry;
+using TextMateSharp.Themes;
+
+namespace CPShare.Models
+{
+    public class CodeToken
+    {
+        public string Text { get; set; }
+        public SKColor ForegroundColor { get; set; }
+        public SKColor BackgroundColor { get; set; }
+    }
+
+    public static class CodeTokenExtend
+    {
+        public static SKImage ToImage(this IEnumerable<CodeToken> codeTokens)
+        {
+            var paint = new SKPaint
+            {
+                Color = SKColors.Red,
+                TextSize = 25,
+                IsAntialias = true,
+                Typeface = SKTypeface.FromFamilyName(Typeface.Default.FontFamily.Name)
+            };
+
+            var wt = 0.0f;
+            var w = 0.0f;
+            var h = 0.0f;
+            var hn = 0;
+            foreach (var token in codeTokens)
+            {
+                foreach (var tokenChar in token.Text)
+                {
+                    if (tokenChar == '\r')
+                        continue;
+                    if (tokenChar == '\n')
+                    {
+                        w = Math.Max(w, wt);
+                        wt = 0;
+                        hn++;
+                        continue;
+                    }
+                    if (tokenChar == ' ')
+                    {
+                        wt += 10;
+                        continue;
+                    }
+                    var tokenStr = tokenChar.ToString();
+
+                    float textWidth = paint.MeasureText(tokenStr);
+                    paint.GetFontMetrics(out var metrics);
+                    float textHeight = metrics.Descent - metrics.Ascent;
+
+                    wt += textWidth;
+                    h = Convert.ToSingle(hn) * textHeight;
+                }
+            }
+
+            w += 50;
+            h += 50;
+            var info = new SKImageInfo(Convert.ToInt32(Math.Round(w)), Convert.ToInt32(Math.Round(h)));
+            var bitmap = new SKBitmap(info);
+            var canvas = new SKCanvas(bitmap);
+            canvas.Clear(SKColors.Black);
+
+            var i = 25.0f;
+            var j = 1;
+            foreach (var token in codeTokens)
+            {
+                foreach (var tokenChar in token.Text)
+                {
+                    if (tokenChar == '\r')
+                        continue;
+                    if (tokenChar == '\n')
+                    {
+                        i = 25.0f;
+                        j++;
+                        continue;
+                    }
+                    if (tokenChar == ' ')
+                    {
+                        i += 10;
+                        continue;
+                    }
+                    var tokenStr = tokenChar.ToString();
+
+                    float textWidth = paint.MeasureText(tokenStr);
+                    paint.GetFontMetrics(out var metrics);
+                    float textHeight = metrics.Descent - metrics.Ascent;
+
+                    paint.Color = token.ForegroundColor;
+                    var font = new SKFont(paint.Typeface, paint.TextSize);
+                    canvas.DrawText(SKTextBlob.Create(tokenStr, font), i, j * textHeight, paint);
+                    i += textWidth;
+                }
+            }
+            return SKImage.FromBitmap(bitmap);
+        }
+
+        public static List<CodeToken> ToCodeTokens(this string code, string codeType)
+        {
+            var ret = new List<CodeToken>();
+            var options = new RegistryOptions(ThemeName.DarkPlus);
+            var registry = new Registry(options);
+            var theme = registry.GetTheme();
+            var grammar = registry.LoadGrammar(options.GetScopeByExtension(codeType));
+
+            ITokenizeLineResult result = grammar.TokenizeLine(code);
+
+            foreach (IToken token in result.Tokens)
+            {
+                var startIndex = (token.StartIndex > code.Length) ?
+                    code.Length : token.StartIndex;
+                var endIndex = (token.EndIndex > code.Length) ?
+                    code.Length : token.EndIndex;
+
+                var foreground = -1;
+                var background = -1;
+                var fontStyle = -1;
+
+                foreach (var themeRule in theme.Match(token.Scopes))
+                {
+                    if (foreground == -1 && themeRule.foreground > 0)
+                        foreground = themeRule.foreground;
+
+                    if (background == -1 && themeRule.background > 0)
+                        background = themeRule.background;
+
+                    if (fontStyle == -1 && themeRule.fontStyle > 0)
+                        fontStyle = themeRule.fontStyle;
+                }
+
+                ret.Add(new CodeToken
+                {
+                    Text = code[startIndex..endIndex],
+                    ForegroundColor = GetColor(foreground, theme),
+                    BackgroundColor = GetColor(background, theme),
+                });
+            }
+            return ret;
+        }
+
+        private static SKColor GetColor(int colorId, Theme theme)
+        {
+            if (colorId == -1)
+                return SKColors.White;
+
+            return SKColor.Parse(theme.GetColor(colorId));
+        }
+    }
+}
